ca665f5610c1578e6e46d0c1d2b33ef4c13557ee
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-02-06 12:53:25 -0500

    Se establecio la cadena de conexion en la dependencia

---
+++ App/IOC/Dependencia.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Repositorios.Contrato;
+using DAL.Repositorios;
+/*using Utility;
+using BLL.Servicios.Contrato;
+using BLL.Servicios;*/
+
+namespace IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SistemaFarmaciaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IVentaRepository, VentaRepository>();
+
+            /*services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            services.AddScoped<IRolService, RolService>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<IVentaService, VentaService>();
+            services.AddScoped<IDashBoardService, DashBoardService>();
+            services.AddScoped<IMenuService, MenuService>();*/
+
+        }
+    }
+}
