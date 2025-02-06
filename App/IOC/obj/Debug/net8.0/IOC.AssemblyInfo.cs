7b369776a998569fc301b805162c9c9e8f707c4a
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-02-06 12:03:28 -0500

    Se agrego las referencias

--- App/IOC/obj/Debug/net8.0/IOC.AssemblyInfo.cs
+++ App/IOC/obj/Debug/net8.0/IOC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a72e1bfe2f936506a2b3c2ccbf197b261bff36")]
 [assembly: System.Reflection.AssemblyProductAttribute("IOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
